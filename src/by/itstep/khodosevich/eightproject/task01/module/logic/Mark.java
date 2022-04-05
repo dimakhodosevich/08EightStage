7e3658df057e011717b3b3258d98b54239962fbe
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-05 22:56:24 +0300

    Added .gitignore and plug methods.

---
+++ src/by/itstep/khodosevich/eightproject/task01/module/logic/Mark.java
@@ -0,0 +1,9 @@
+package by.itstep.khodosevich.eightproject.task01.module.logic;
+
+public class Mark {
+
+    public static String getStringEquivalentOfMark(int mark){
+
+        return "";
+    }
+}
---
+++ test/by.itstep.khodosevich.eightproject.task01/TestMark.java
@@ -0,0 +1,11 @@
+package by.itstep.khodosevich.eightproject.task01;
+
+import org.junit.Test;
+
+public class TestMark {
+
+    @Test
+    public void testGetStringEquivalentOfMark(){
+
+    }
+}
